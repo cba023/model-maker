2fe4a98e4503a91ee7a2919d8bfb9434f5065687
chenbo <chenbo@wekoi.cn>
2025-05-19 14:52:39 +0800

    优化

--- model_maker/macos/Runner/MainFlutterWindow.swift
+++ model_maker/macos/Runner/MainFlutterWindow.swift
@@ -5,6 +5,7 @@ class MainFlutterWindow: NSWindow {
     override func awakeFromNib() {
         self.minSize = NSSize(width: 1080, height: 1080 * 0.65)
         let flutterViewController = FlutterViewController()
+        self.setContentSize(self.minSize)
         let windowFrame = self.frame
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
