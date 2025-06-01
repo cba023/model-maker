7c0223631786a19d7b042f5a1af80b9e6bdd8ea7
chenbo <chenbo@wekoi.cn>
2025-06-01 17:29:39 +0800

    功能优化

--- model_maker/macos/Runner/AppDelegate.swift
+++ model_maker/macos/Runner/AppDelegate.swift
@@ -3,11 +3,22 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+    
+    @IBAction func openRespUrl(_ sender: Any) {
+        if let url = URL(string: "https://gitlab.wekoi.cc/wtc/wl-client/model_maker") {
+            NSWorkspace.shared.open(url)
+        }
+    }
+    @IBAction func openGiteeUrl(_ sender: Any) {
+        if let url = URL(string: "https://gitee.com/cba023/model-maker") {
+            NSWorkspace.shared.open(url)
+        }
+    }
 }
